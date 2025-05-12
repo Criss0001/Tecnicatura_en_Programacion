cb5cf113ae42c045db7794e7ba266441e9fb6a45
Cristian Farina <cristianjfarina@gmail.com>
2025-05-12 18:49:13 -0300

    Ingresamosel proyecto de Python al repositorio

---
+++ Python/Leccion_2/main.py
@@ -0,0 +1,9 @@
+#En ésta clase veremos la sentecncia if/else
+condicion = 10
+if condicion == True:
+    print('Condición Verdadera')
+elif condicion == False:
+    print('Condición Falsa')
+else:
+    print('Condición sin especificar')
+
