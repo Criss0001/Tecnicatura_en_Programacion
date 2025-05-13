631a2b5c05b71bd783dd489e27b0040a022ed2f6
Cristian Farina <cristianjfarina@gmail.com>
2025-05-13 12:18:44 -0300

    Segundo ejercicio con cadenas en js concatenacion

---
+++ JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,15 @@
+var nombre = 'Cristian ';
+var apellido = 'Farina';
+var nombreCompleto = nombre+' '+apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = 'Jesus'+' '+'Farina';
+console.log(nombreCompleto2);
+var juntos = nombre + 219; //Lee de izq a der siguiendo la cadena como string
+console.log(juntos);
+juntos = nombre + 78 + 17; //Aquí l
+console.log(juntos)
+juntos = 78 + 17 + nombre;
+console.log(juntos);
+
+nombre += apellido; //Tercera Concatenación usando el operador simplificado
+console.log(nombre);
