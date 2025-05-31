8ac56c85111c9e09c9ca95daf1ecb74ae2672bde
Cristian Farina <cristianjfarina@gmail.com>
2025-05-31 09:19:44 -0300

    Agregamos ejercicios par,edad

---
+++ JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar números pares
+let parImpar = 7;
+if(parImpar % 2 == 0){
+    console.log("Es un número PAR");
+}
+else(
+    console.log("Es un número IMPAR")
+)
+
+//Ejercicio: es mayor de edad
+let edad = 18, adulto = 18;
+if(edad >= adulto){
+    console.log("Usted es mayor de edad");
+}
+else{
+    console.log("Usted es menor de edad");
+}   
