e33195c6d342ea1abc9662740e0d8b4df3e3bb32
Cristian Farina <cristianjfarina@gmail.com>
2025-09-04 09:44:45 -0300

    Agregue Clases de leccion 3 y 4

---
+++ Java/Leccion3/leccionClasesObjetos/src/Clases/Creacion_de_un_Objeto.java
@@ -0,0 +1,13 @@
+
+package Clases;
+
+public class Creacion_de_un_Objeto {
+    public static void main(String[] args) {
+        Persona persona1;
+        persona1 = new Persona();//Llamamos al constructor
+        persona1.nombre = "Ariel";
+        persona1.apellido = "Betancud";
+        persona1.obtenerInformacion();
+        
+    }
+}
