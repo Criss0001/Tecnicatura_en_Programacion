82cfbee1f2c3e630e0082a22856acdcec4bad419
Cristian Farina <cristianjfarina@gmail.com>
2025-10-22 12:43:37 -0300

    Agregamos clase 10 JS

---
+++ JavaScript/10.4_Creamos_la_Clase_Orden_Parte_1_y_2.js
@@ -0,0 +1,68 @@
+class Producto{
+
+    static contadorProductos = 0;
+    constructor(nombre, precio){
+        this._idProducto = ++Producto.contadorProductos;
+        this._nombre = nombre;
+        this._precio = precio;
+    }
+
+    get idProducto(){
+        return this._idProducto;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get precio(){
+        return this._precio;
+    }
+
+
+    set precio(precio){
+        this._precio = precio;
+    }
+
+    toString(){ //Template Literals: Nos permite insertar código dinamicamente
+        return `idProducto: ${this._idProducto}, nombre: ${this._nombre}, precio: $${this._precio}`;
+    }
+
+} //Fin de la clase Producto
+
+class Orden{
+
+    static contadorOrdenes = 0;
+    static getMAX_PRODUCTOS(){
+        return 5;
+    }
+
+    constructor(){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = [];
+        this._contadorProductosAgregados = 0;
+    }
+
+    get idOrden(){
+        return this._idOrden;
+    }
+
+    agregarProducto(producto){
+        if(this._productos.length < Orden.getMAX_PRODUCTOS()){
+            this._productos.push(producto);//Tenemos 2 tipos de sintáxis: 1
+            this._productos[this._contadorProductosAgregados++] = producto; //segunda sintáxis
+        } else {
+            console.log('No se pueden agregar más productos');
+        }
+    }//Fin del método agregarproducto
+}//Fin de la clase Orden
+
+
+let producto1 = new Producto('Pantalón', 200);
+let producto2 = new Producto('Camisa', 150);
+console.log(producto1.toString());
+console.log(producto2.toString());
